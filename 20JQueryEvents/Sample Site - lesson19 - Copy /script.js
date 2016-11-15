b7cb605cf88292c562c449e6cf6ace7a3e0ce873
Brendan Greene <brendanjgreene@gmail.com>
2016-11-15 22:31:52 +0000

    challenge a of button.html

--- 20JQueryEvents/Sample Site - lesson19 - Copy /script.js
+++ 20JQueryEvents/Sample Site - lesson19 - Copy /script.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
  //
  // add your jQuery code here
+ $('#button1').mouseenter(function() {
+ 	$('#button1').removeClass("makeRed").addClass("makeBorder");
+ });
+
+ $('#button1').mouseleave(function() {
+ 	$('#button1').addClass("makeRed").removeClass("makeBorder");
+ });
 
 
 }); 
