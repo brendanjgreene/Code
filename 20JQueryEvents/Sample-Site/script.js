77fc584613c57f811e1d099037c6947f4740ecb0
Brendan Greene <brendanjgreene@gmail.com>
2016-11-15 20:57:01 +0000

    sample site added and script and css added for first two elements of challenge

---
+++ 20JQueryEvents/Sample-Site/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+
+ 	$("p").click(function(){
+        $("p").addClass( "highlight_text");
+    });
+
+    $("h2").hover(function(){
+    	$("h2").addClass( "lightBlueBack");
+    });
+
+
+}); 
