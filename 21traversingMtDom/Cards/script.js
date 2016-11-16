1698a697d5829e585e2e32db140c38a984c6f140
Brendan Greene <brendanjgreene@gmail.com>
2016-11-16 16:31:22 +0000

    remove script which was interfering with highlighted card class

--- 21traversingMtDom/Cards/script.js
+++ 21traversingMtDom/Cards/script.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
-    $(".stream-nav").on("click", function() {
-        /**
-         * When we click on an element that has
-         * a 'box' class, this event will be fired.
-         */
-        var elementId = $(this).attr("id");
-        var cardClass = $(".card").attr("class").split(" ")[0];
-
-        if ($("." + elementId).css("background-color") == "rgb(235, 82, 85)") {
-            $("." + elementId).css("background-color", "#fff");
-        } else {
-            $("." + cardClass).css("background-color", "#fff");
-            $("." + elementId).css("background-color", "rgb(235, 82, 85)");
-        }
-    });
+    
 
     $("p").click(function(){
         $(this).children("a").css("background-color", "yellow");
@@ -29,5 +15,13 @@ $(document).ready(function() {
 
     $(".card").click(function(){
         $(this).toggleClass("highlighted_card");
+    });
+
+    $("#select").click(function(){
+        $(".card:not(.highlighted_card)").hide();
+    });
+
+    $("#all").click(function(){
+        $(".card").show();
     });
 });
