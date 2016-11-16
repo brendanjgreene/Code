adead75e0478408a9e530d698b18fd2a5f945292
Brendan Greene <brendanjgreene@gmail.com>
2016-11-16 17:38:43 +0000

    alternative solution do dehighlighting rows

--- 21traversingMtDom/tableProject/js/my_script.js
+++ 21traversingMtDom/tableProject/js/my_script.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 	//$("tr:odd").addClass("odd-color")
 	//$("tr:even").addClass("even-color")
 	$("th").click(function(){
-		$("tr").children().removeClass("highlight");
+		//$("tr").children().removeClass("highlight");
+		$("td").removeClass("highlight"); // alternative solution
 		$(this).siblings().addClass("highlight");
 	});
 });
