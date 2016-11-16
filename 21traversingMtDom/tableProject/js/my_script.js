d3dde2ee7fccd942edead96bf90ef2de29b03064
Brendan Greene <brendanjgreene@gmail.com>
2016-11-16 17:36:11 +0000

    remove highlighted class from other rows when row is clicked

--- 21traversingMtDom/tableProject/js/my_script.js
+++ 21traversingMtDom/tableProject/js/my_script.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
-	$("tr:odd").addClass("odd-color")
-	$("tr:even").addClass("even-color")
+	//$("tr:odd").addClass("odd-color")
+	//$("tr:even").addClass("even-color")
+	$("th").click(function(){
+		$("tr").children().removeClass("highlight");
+		$(this).siblings().addClass("highlight");
+	});
 });
