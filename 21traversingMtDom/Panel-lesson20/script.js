e5446c50621254dba9e2722d9095e32317a9cccb
Brendan Greene <brendanjgreene@gmail.com>
2016-11-16 17:06:56 +0000

    two solutions for fading all panels to 10prct

--- 21traversingMtDom/Panel-lesson20/script.js
+++ 21traversingMtDom/Panel-lesson20/script.js
@@ -3,7 +3,13 @@ $(document).ready(function(){
  //$("#panel").click(function(){
  //	$("#panel").hide();
  //});
- $('.theButton').click(function(){
- 	$(this).hide();
+ //$('.theButton').click(function(){
+ //	$(this).hide();
+ //});
+ //$("#panel").click(function(){
+ 	//$("#panel").fadeTo(1000, 0.1);
+ //}); panel solution works just as well when selecting all
+ $(".theButton").click(function(){
+ 	$("#panel .container").siblings().fadeTo(1000, 0.1);
  });
 });
