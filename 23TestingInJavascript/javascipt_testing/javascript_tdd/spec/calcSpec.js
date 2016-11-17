9080d42cf484cf1f350056521b2a0ec7727136e5
Brendan Greene <brendanjgreene@gmail.com>
2016-11-17 18:27:44 +0000

    add spy to calcspec.js

--- 23TestingInJavascript/javascipt_testing/javascript_tdd/spec/calcSpec.js
+++ 23TestingInJavascript/javascipt_testing/javascript_tdd/spec/calcSpec.js
@@ -7,5 +7,11 @@ describe("Calculator", function() {
         it("should not return 4 if the parameters given don't add up to 4", function(){
         	expect(addition(7, 19)).toBe(26);
         });
+
+        it("should have called the alert function if either number is undefined", function() {
+            spyOn(window, "alert");
+            addition("Hello");
+            expect(window.alert).toHaveBeenCalledWith("Arguments must both be a number");
+        });
     });
 });
