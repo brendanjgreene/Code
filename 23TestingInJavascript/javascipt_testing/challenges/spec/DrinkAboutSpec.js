63d7db6ff768951aa348b5fb2fbd80b0a3273c25
Brendan Greene <brendanjgreene@gmail.com>
2016-11-18 21:21:31 +0000

    finished drink about challenge

--- 23TestingInJavascript/javascipt_testing/challenges/spec/DrinkAboutSpec.js
+++ 23TestingInJavascript/javascipt_testing/challenges/spec/DrinkAboutSpec.js
@@ -1,25 +1,50 @@
 describe("whatCanIDrink", function() {
  
     beforeEach(function() {
-        calc = new whatCanIDrink();
+        age = new whatCanIDrink();
     });
-    describe('Addition function', function() {
-        it('should add two numbers together and return the result', function() {
-            calc.add(2);
-            calc.add(2);
-            expect(calc.value).toBe(4);
-        });
- 
-        it("should not return 4 if the numbers given don't add up to 4", function() {
-            calc.add(12);
-            calc.add(7);
-            expect(calc.value).toBe(19);
-        });
- 
-        it("should have called the alert function if the number given is undefined", function() {
+    describe('under 0 function', function() {
+        it('should return "Sorry. I can’t tell what drink because that age is incorrect!"', function() {
             spyOn(window, "alert");
-            calc.add();
-            expect(window.alert).toHaveBeenCalledWith("Argument must be a number");
+            age.add(-5);
+            expect(window.alert).toHaveBeenCalledWith("Sorry. I can’t tell what drink because that age is incorrect!");
         });
     });
+    describe('under 14 function', function() {
+        it('should return "Drink Toddy"', function() {
+            spyOn(window, "alert");
+            age.add(11);
+            expect(window.alert).toHaveBeenCalledWith("Drink Toddy");
+        });
+    });
+    describe('under 18 function', function() {
+        it('should return "Drink Coke"', function() {
+            spyOn(window, "alert");
+            age.add(16);
+            expect(window.alert).toHaveBeenCalledWith("Drink Coke");
+        });
+    });
+    describe('under 21 function', function() {
+        it('should return "Drink Beer"', function() {
+            spyOn(window, "alert");
+            age.add(19);
+            expect(window.alert).toHaveBeenCalledWith("Drink Beer");
+        });
+    });
+    describe('under 130 function', function() {
+        it('should return "Drink Whisky"', function() {
+            spyOn(window, "alert");
+            age.add(45);
+            expect(window.alert).toHaveBeenCalledWith("Drink Whisky");
+        });
+    });
+    describe('too Old function', function() {
+        it('should return "Sorry etc...."', function() {
+            spyOn(window, "alert");
+            age.add(132);
+            expect(window.alert).toHaveBeenCalledWith("Sorry. I can't tell what drink because the age is incorrect!");
+        });
+    });
+
+
 });
--- 23TestingInJavascript/javascipt_testing/challenges/src/DrinkAbout.js
+++ 23TestingInJavascript/javascipt_testing/challenges/src/DrinkAbout.js
@@ -1,11 +1,22 @@
 whatCanIDrink = function() {
-    this.value = 0;
+ 	this.age = 0;
 };
- 
+
 whatCanIDrink.prototype.add = function(number) {
-    if (typeof(number) == "number") {
-        this.value += number;
-    } else {
-        alert("Argument must be a number");
+	if (this.age+number<0){
+    alert("Sorry. I can’t tell what drink because that age is incorrect!");
+	}
+   	else if (this.age+number<14){
+    alert("Drink Toddy");
     }
+    else if (this.age+number<18){
+    alert("Drink Coke");
+    }
+    else if (this.age+number<21){
+    alert("Drink Beer");
+    }
+    else if (this.age+number<130){
+    alert("Drink Whisky");
+    }
+    else alert("Sorry. I can't tell what drink because the age is incorrect!");
 };
