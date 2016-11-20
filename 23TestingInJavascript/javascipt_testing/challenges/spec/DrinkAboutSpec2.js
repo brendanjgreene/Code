da089b878322d669dc658ee58832f50a2f758f52
Brendan Greene <brendanjgreene@gmail.com>
2016-11-20 11:33:38 +0000

    add new version of drink about which i think works better

---
+++ 23TestingInJavascript/javascipt_testing/challenges/spec/DrinkAboutSpec2.js
@@ -0,0 +1,50 @@
+describe("whatCanIDrink", function() {
+ 
+    beforeEach(function() {
+        age = new whatCanIDrink();
+    });
+    describe('under 0 function', function() {
+        it('should return "Sorry. I can’t tell what drink because that age is incorrect!"', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(27);
+            expect(window.alert).toHaveBeenCalledWith("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+    });
+    describe('under 14 function', function() {
+        it('should return "Drink Toddy"', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(11);
+            expect(window.alert).toHaveBeenCalledWith("Drink Toddy");
+        });
+    });
+    describe('under 18 function', function() {
+        it('should return "Drink Coke"', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(16);
+            expect(window.alert).toHaveBeenCalledWith("Drink Coke");
+        });
+    });
+    describe('under 21 function', function() {
+        it('should return "Drink Beer"', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(19);
+            expect(window.alert).toHaveBeenCalledWith("Drink Beer");
+        });
+    });
+    describe('under 130 function', function() {
+        it('should return "Drink Whisky"', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(45);
+            expect(window.alert).toHaveBeenCalledWith("Drink Whisky");
+        });
+    });
+    describe('too Old function', function() {
+        it('should return "Sorry etc...."', function() {
+            spyOn(window, "alert");
+            whatCanIDrink(132);
+            expect(window.alert).toHaveBeenCalledWith("Sorry. I can't tell what drink because the age is incorrect!");
+        });
+    });
+
+
+});
---
+++ 23TestingInJavascript/javascipt_testing/challenges/src/DrinkAbout2.js
@@ -0,0 +1,21 @@
+whatCanIDrink = function(number) {
+    this.age = 0;
+    if (this.age+number<0){
+    alert("Sorry. I can’t tell what drink because that age is incorrect!");
+    }
+    else if (this.age+number<14){
+    alert("Drink Toddy");
+    }
+    else if (this.age+number<18){
+    alert("Drink Coke");
+    }
+    else if (this.age+number<21){
+    alert("Drink Beer");
+    }
+    else if (this.age+number<130){
+    alert("Drink Whisky");
+    }
+    else alert("Sorry. I can't tell what drink because the age is incorrect!");
+};
+
+whatCanIDrink
